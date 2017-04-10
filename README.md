092a8e1ba53db98ed1e7c50d767e50eecbe273dc
昊川 <gonghao.gh@alibaba-inc.com>
2017-04-10 11:41:11 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# spm-loader
-auto create traceless goldlog
+<div align="center">
+  <a href="https://github.com/webpack/webpack">
+    <img width="200" height="200"
+      src="https://webpack.js.org/assets/icon-square-big.svg">
+  </a>
+<h1>SPM Loader</h1>
+</div>
+** Note: based on alibaba aplus data framework, only support aplus traceless goldlog, and is strongly dependent on React !!**
+<h2 align="center">Install</h2>
+```bash
+npm install --save-dev spm-loader
+```
+<h2 align="center">Usage</h2>
+
+### configuration (use it combined with babel-loader)
+```js
+module.exports = {
+	module: {
+    	rules: [{
+        	test: /.\jsx?$/,
+            use: [
+            	'babel-loader',
+                'spm-loader'
+            ]
+        }]
+    }
+}
+```
+
+### Options
+#### `context`
+In jsx, we use `context` as the context of any iteration, so we can get properties in iteration, default is `spm_self`.
+
+#### `key`
+A module can be used many times in a parent module, but the traceless goldlog key must be unique, so `key` is used to identity a module, default is `spmKey`.
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const loaderUtils = require('loader-utils');
+const REG = /spm-auto-click\s*(=\s*\{.*?\})?/g;
+const ENTRY = /(render\s*\([^\)]*\)\s*\{)/;
+const defaultOptions = {
+  context: 'spm_self',
+  key: 'spmKey'
+};
+
+const findEntry = function(mod) {
+  if (mod.reasons.length > 0) {
+    var mods = mod.resource.split('/');
+    return mods[mods.length - 1].split('.')[0];
+  }
+  return '';
+};
+
+module.exports = function(source, inputSourceMap) {
+  const customOptions = loaderUtils.parseQuery(this.query);
+  const options = Object.assign({}, defaultOptions, customOptions);
+  const name = findEntry(this._module);
+  this.cacheable();
+  if (source.indexOf('spm-auto-click') > -1) {
+    source = source.replace(ENTRY, function(word) {
+      return `${word}\nconst ${options.context} = this;`;
+    }).replace(REG, function(all, match) {
+      var key = match && match.match(/{([\w\W]+)}/);
+      if (key && key[1]) {
+        return `data-spm-click={"gostr=/aliyun;locaid=d${name}"+(${options.context}.props.${options.key} || \'\')+${key[1]}}`;
+      } else {
+        return `data-spm-click={"gostr=/aliyun;locaid=d${name}"+(${options.context}.props.${options.key} || \'\')}`;
+      }
+    });
+  }
+  this.callback(null, source, inputSourceMap);
+};
